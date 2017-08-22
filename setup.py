6cbe0e4c5be2a73d0aa4b1c74484bda0f1b3296a
Zack P <zack@zacklearns.com>
2017-08-21 18:43:47 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+octo = path.abspath(path.dirname(__file__))
+with open(path.join(octo, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+  name='octo-adv',
+  version='1.0',
+  
+  description='An experimental image replicator based off hyperGAN.',
+  long_description=long_description,
+  
+  url='https://github.com/source-live/octo-adv',
+  
+  author='Source Live',
+  author_email='zack@zacklearns.com',
+  
+  license='MIT',
+  
+  classifiers=[
+    'Development Status :: 1 - Pre-Alpha',
+    'Intended Audience :: Developers',
+    'Environment :: Console',
+    'License :: OSI Approved :: MIT License',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Multimedia :: Graphics'
+  ]
+  
+  keywords='generate art teach computer graphics random code python experimental',
+  )
